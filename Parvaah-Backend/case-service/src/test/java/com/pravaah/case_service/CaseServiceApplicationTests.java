8e9a8e4b0db28faef845166c55f04a95b9e9f9a0
tushar-dot <devtushar30@gmail.com>
2026-01-20 22:35:20 +0530

    case-service added

---
+++ Parvaah-Backend/case-service/src/test/java/com/pravaah/case_service/CaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravaah.case_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
